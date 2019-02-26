bc95a466daa5167ac711e190648c785b2678a585
amanda <am.morang@gmail.com>
2019-02-25 20:53:25 -0500

    user must enter user name, removed errors from console if user clicks cancel on the prompt

--- script2.js
+++ script2.js
@@ -2,16 +2,22 @@
 
     function startGame() {
         let answer = prompt("Would you like to play a game?");
-        if (answer.toLowerCase() === "yes") {
-            let userName = prompt("Enter your character name!");
+
+        if (!answer) {
+            console.log("Aww, not even once?");
+        } else if (answer.toLowerCase() === "yes") {
+            let userName = prompt("Enter your character's name!");
+            while (!userName) {
+                userName = prompt("You need to give your character a name - enter something!");
+            }
             startCombat(userName);
         } else if (answer.toLowerCase() === "no") {
             console.log("That's okay, maybe we can play later?");
-        }
+        } 
     }
 
     function getDamage() {
-        return Math.floor(Math.random() * 5 + 1)
+        return Math.floor(Math.random() * 5 + 1);
     }
 
     function startCombat(name) {
@@ -19,9 +25,14 @@
         let grantHealth = 10;
         let wins = 0;
         console.log(`${name}'s Health: ${userHealth}, Grant's Health: ${grantHealth}, ${name}'s Wins: ${wins}`);
+        
         while (grantHealth > 0) {
             let userMove = prompt("Would you like to attack or quit?");
-            if (userMove === "attack") {
+            if (!userMove) {
+                console.log("Aww, don't give up so soon!");
+                break;
+            } 
+            if (userMove.toLowerCase() === "attack") {
                 console.log(`${name}'s health is now: ${userHealth -= getDamage()}`);
                 console.log(`Grant's Health is now: ${grantHealth -= getDamage()}`);
                 if (userHealth <= 0) {
@@ -33,12 +44,14 @@
                         console.log("You beat Grant! You are the best!");
                         break;
                     }
-                    console.log(`${name} wins: ${wins}`);
+                    console.log(`Games ${name} has won: ${wins}. Keep fighting!`);
                     grantHealth = 10;
                 }
             } else if (userMove.toLowerCase() === "quit") {
                 console.log("Hey, maybe next time!");
                 break;
+            } else {
+                prompt("I don't understand. Type in attack or quit.")
             }
         }
     }
